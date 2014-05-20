6d5146bc79ab053e7fc19c8ecad7d5b49a140e88
Xavier <xavier.gibert@c-s.fr>
2014-05-20 12:40:23 +0200

    Multi-dimnsional menu for views working, going to remove spinner

--- src/fragments/HudFragment.java
+++ src/fragments/HudFragment.java
@@ -12,14 +12,20 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.PopupMenu.OnMenuItemClickListener;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.SlidingDrawer;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -55,7 +61,8 @@ public final class HudFragment extends Fragment implements OnItemSelectedListene
 	private Simulator simulator;
 	XWalkView browser;
 	LinearLayout browserLayout, slider_content;
-
+	Button views_menu;
+	
 	@SuppressWarnings("deprecation")
 	@SuppressLint({ "JavascriptInterface", "SetJavaScriptEnabled", "NewApi" })
 	@Override
@@ -107,7 +114,7 @@ public final class HudFragment extends Fragment implements OnItemSelectedListene
     	browserSettings.setAllowUniversalAccessFromFileURLs(true);
     	//browserSettings.setBuiltInZoomControls(true);
     	//browserSettings.setDisplayZoomControls(true);
-    	//browserSettings.setSupportZoom(true);
+    	//browserSettings.setSupportZoom(true);, OnMenuItemClickListener
     	
     	browser.clearCache(true);
     	
@@ -128,6 +135,15 @@ public final class HudFragment extends Fragment implements OnItemSelectedListene
     	
     	Spinner spinner = (Spinner) rootView.findViewById(R.id.spinnerView);
     	spinner.setOnItemSelectedListener(this);
+    	
+    	views_menu = (Button) rootView.findViewById(R.id.buttonViews);
+    	views_menu.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				showPopup(arg0);
+			}
+    		
+    	});
     	
     	//Play/Pause/Stop buttons
     	ImageButton but_play = (ImageButton)rootView.findViewById(R.id.imageButtonPlay);
@@ -176,6 +192,64 @@ public final class HudFragment extends Fragment implements OnItemSelectedListene
         // Another interface callback
     }
  
+    private void showPopup(View v) {
+    	PopupMenu popup = new PopupMenu(getActivity(), v);
 
+        // This activity implements OnMenuItemClickListener
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+        	@Override
+            public boolean onMenuItemClick(MenuItem item) {
+        		String com_view = (String)item.getTitle();
+                switch (item.getItemId()) {
+                    case R.id.menu_views_earth:
+                    	break;
+                    case R.id.menu_views_sun:
+                    	break;
+                    case R.id.menu_views_ref_frame_xyz:
+                    	break;
+                    case R.id.menu_views_ref_frame_x:
+                    	break;
+                    case R.id.menu_views_ref_frame_xx:
+                    	break;
+                    case R.id.menu_views_ref_frame_y:
+                    	break;
+                    case R.id.menu_views_ref_frame_yy:
+                    	break;
+                    case R.id.menu_views_ref_frame_z:
+                    	break;
+                    case R.id.menu_views_ref_frame_zz:
+                    	break;
+                    case R.id.menu_views_spacecraft_xyz:
+                    	com_view = "S/C-" + com_view;
+                    	break;
+                    case R.id.menu_views_spacecraft_rear:
+                    	com_view = "S/C-" + com_view;
+                    	break;
+                    case R.id.menu_views_spacecraft_front:
+                    	com_view = "S/C-" + com_view;
+                    	break;
+                    case R.id.menu_views_spacecraft_top:
+                    	com_view = "S/C-" + com_view;
+                    	break;
+                    case R.id.menu_views_spacecraft_bottom:
+                    	com_view = "S/C-" + com_view;
+                    	break;
+                    case R.id.menu_views_spacecraft_left:
+                    	com_view = "S/C-" + com_view;
+                    	break;
+                    case R.id.menu_views_spacecraft_right:
+                    	com_view = "S/C-" + com_view;
+                    	break;
+                    default:
+                        return false;
+                }
+                views_menu.setText(com_view);
+                browser.load("javascript:changeView('"+com_view+"')", null);
+                return true;
+            }
+        });
+        popup.inflate(R.menu.views);
+        popup.show();
 
+    }
 }
