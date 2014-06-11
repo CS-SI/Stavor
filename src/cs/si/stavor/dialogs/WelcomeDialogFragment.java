65c7fb2944a44550df32aa08a97b7b26e5721492
Xavier <xavier.gibert@c-s.fr>
2014-06-11 10:57:11 +0200

    welcome dialog beauty

--- src/cs/si/stavor/dialogs/WelcomeDialogFragment.java
+++ src/cs/si/stavor/dialogs/WelcomeDialogFragment.java
@@ -6,7 +6,10 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.TextView;
 
 /**
  * Welcome dialog to inform of the installation paths
@@ -19,8 +22,24 @@ public class WelcomeDialogFragment extends DialogFragment {
         // Use the Builder class for convenient dialog construction
 		String data_path = Installer.getOrekitDataRoot(this.getActivity()).getPath();
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        
+        // Get the layout inflater
+        //LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        //builder.setView(inflater.inflate(R.layout.welcome, null));
+        builder.setIcon(R.drawable.ic_launcher);
+        TextView tx_path = new TextView(getActivity());
+        tx_path.setText(data_path);
+        tx_path.setTypeface(null,Typeface.ITALIC);
+        int padding_in_dp = 20;  // 6 dps
+        final float scale = getResources().getDisplayMetrics().density;
+        int padding_in_px = (int) (padding_in_dp * scale + 0.5f);
+        tx_path.setPadding(padding_in_px, 0, padding_in_px, 0);
+        builder.setView(tx_path);
         builder.setTitle(getString(R.string.dialog_welcome))
-        		.setMessage(getString(R.string.dialog_install_message)+" "+data_path)
+        		.setMessage(getString(R.string.dialog_install_message))
                .setPositiveButton(getString(R.string.dialog_continue), new DialogInterface.OnClickListener() {
                    public void onClick(DialogInterface dialog, int id) {
                        // Dummy
