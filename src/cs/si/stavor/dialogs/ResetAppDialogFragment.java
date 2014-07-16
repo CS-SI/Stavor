261792613914466840ae136b96647bee53abcac6
Xavier <xavier.gibert@c-s.fr>
2014-07-16 17:01:10 +0200

    Layouts fix, mostly tablet ones

--- src/cs/si/stavor/dialogs/ResetAppDialogFragment.java
+++ src/cs/si/stavor/dialogs/ResetAppDialogFragment.java
@@ -1,15 +1,10 @@
 package cs.si.stavor.dialogs;
 
 import cs.si.stavor.R;
-import cs.si.stavor.MainActivity;
-import android.app.AlarmManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
