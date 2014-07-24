4e15a01c2bac92a2f7992c388cde2755f7f7b9e8
Xavier <xavier.gibert@c-s.fr>
2014-07-24 09:59:49 +0200

    checkpoint replace negative positive areas by just date line jumps

--- src/cs/si/stavor/app/Parameters.java
+++ src/cs/si/stavor/app/Parameters.java
@@ -74,7 +74,7 @@ public class Parameters {
 	}
 	public static class Map {
 		public static final double marker_pos_threshold = 0.2;//In deg
-		public static final int station_visibility_points = 50;//Points in the polygon
+		public static final int station_visibility_points = 30;//Points in the polygon
 		public static final int satellite_fov_points = 5;
 	}
 }
