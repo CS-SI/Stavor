ea1421ee3003cf423793a40f05112eb9b4e7d279
unknown <xgibert@PO13917.IDSI0.si.c-s.fr>
2015-01-27 18:23:13 +0100

    Starting re-structuration of web assets: joint all visualization modules together in one page

---
+++ stavor/src/main/assets/www2/js/OpenLayers/mobile.js
@@ -0,0 +1,49 @@
+// initialize map when page ready
+var map;
+
+// Get rid of address bar on iphone/ipod
+var fixSize = function() {
+    window.scrollTo(0,0);
+    document.body.style.height = '100%';
+    if (!(/(iphone|ipod)/.test(navigator.userAgent.toLowerCase()))) {
+        if (document.body.parentNode) {
+            document.body.parentNode.style.height = '100%';
+        }
+    }
+};
+setTimeout(fixSize, 700);
+setTimeout(fixSize, 1500);
+
+var init = function () {
+    // create map
+    map = new OpenLayers.Map({
+        div: "map",
+        theme: null,
+        numZoomLevels: 18,
+        controls: [
+            new OpenLayers.Control.TouchNavigation({
+                dragPanOptions: {
+                    enableKinetic: true
+                }
+            })
+        ],
+        layers: [
+            new OpenLayers.Layer.OSM("OpenStreetMap", null, {
+                transitionEffect: 'null',
+		wrapDateLine: false
+            })
+        ],
+        center: new OpenLayers.LonLat(0, 0),
+        zoom: 0
+    });
+
+	/*map.events.register("moveend", map, function(){
+	     var lyr = map.getLayersByName('vectorLayer')[0];
+	     map.removeLayer(lyr);
+	});*/
+
+	map.events.register("zoomend", map, function(){
+	     storeNewZoom(map.getZoom());
+	});
+
+};
