e8f6c12ab3846199055dda6188324d16918b5d52
unknown <xgibert@PO13917.IDSI0.si.c-s.fr>
2015-02-02 18:28:51 +0100

    preparing... Mission list

---
+++ stavor/src/main/assets/www2/modules/simulator/components/Mission.js
@@ -0,0 +1,13 @@
+var Mission = function () {
+	this._id = 0;
+	this.name = "CustomMission";
+	this.description = "";
+	this.duration = 600000.0;
+	this.step = 60.0;
+	this.initial_date = new SimDate();
+	this.initial_mass = 2000.0;
+	this.initial_orbit = new Orbit();
+}
+
+
+
---
+++ stavor/src/main/assets/www2/modules/simulator/components/Orbit.js
@@ -0,0 +1,28 @@
+var Orbit = function(){
+	
+	this.mu =  3.986004415e+14;
+	/**
+	 * Semi major axis in meters
+	 */
+	this.a = 24396159;
+	/**
+	 * Eccentricity
+	 */
+	this.e = 0.72831215;
+	/**
+	 * Inclination
+	 */
+	this.i = Math.PI*7/180;//rad
+	/**
+	 * Perigee argument
+	 */
+	this.omega = Math.PI;//rad
+	/**
+	 * Right ascension of ascending node
+	 */
+	this.raan = 0;//rad
+	/**
+	 * Mean anomaly
+	 */
+	this.lM = 0;
+}
