3e6e34811f3c602a555a2216f4c74db1090ae6fa
unknown <xgibert@PO13917.IDSI0.si.c-s.fr>
2015-02-20 10:28:19 +0100

    Clean stavor3 GUI from assets

--- stavor/src/main/assets/www2/js/Stavor/database.js
+++
@@ -1,9 +0,0 @@
-
-//Database
-var db = openDatabase('stavor', '1.0', 'Stavor database', 2 * 1024 * 1024);
-
-function successDatabaseHandler(){
-}
-function errorDatabaseHandler(transaction, error) {
-	if(parameters.debug) alert("Error : " + error.message);
-}
--- stavor/src/main/assets/www2/js/Stavor/parameters.js
+++
@@ -1,6 +0,0 @@
-var parameters = {
-	version: "3.0.0",
-	debug: true,
-	splash_min_time: 3000,
-	simulator_target_fps: 25
-} 
