914ba4878400952edfeaceba75ddf7e87285acb2
unknown <xgibert@PO13917.IDSI0.si.c-s.fr>
2015-02-24 17:01:27 +0100

    New Stavor Dialog icons

--- stavor/src/main/assets/www/js/Stavor/Dialog.js
+++ stavor/src/main/assets/www/js/Stavor/Dialog.js
@@ -7,14 +7,16 @@ Dialog.showDialog = function(title, message, callbackOk){
 	this.dialog_title = document.getElementById("DivDialogTitle");
 	this.dialog_text = document.getElementById("DivDialogText");
 	this.confirm_button = document.getElementById("DialogConfirmButton");
-	this.cancel_button = document.getElementById("DialogCloseButton");
+	this.cancel_button = document.getElementById("DialogCancelButton");
+	this.ok_button = document.getElementById("DialogCloseButton");
 	
 	//Show dialog
 	this.dialog_title. innerHTML = title;
 	this.dialog_text. innerHTML = message;
 	this.confirm_button.style.display = "none";
-	this.cancel_button.style.display = "block";
-	this.cancel_button.onclick = function(){	
+	this.cancel_button.style.display = "none";
+	this.ok_button.style.display = "block";
+	this.ok_button.onclick = function(){	
 		callbackOk();
 		$( "#DivDialogBackground" ).fadeOut( "fast", function() {});
 	};
@@ -27,11 +29,13 @@ Dialog.showConfirmDialog = function(title, message, callbackOk, callbackCancel){
 	this.dialog_title = document.getElementById("DivDialogTitle");
 	this.dialog_text = document.getElementById("DivDialogText");
 	this.confirm_button = document.getElementById("DialogConfirmButton");
-	this.cancel_button = document.getElementById("DialogCloseButton");
+	this.cancel_button = document.getElementById("DialogCancelButton");
+	this.ok_button = document.getElementById("DialogCloseButton");
 	
 	//Show confirm dialog
 	this.dialog_title. innerHTML = title;
 	this.dialog_text. innerHTML = message;
+	this.ok_button.style.display = "none";
 	this.cancel_button.style.display = "block";
 	this.cancel_button.onclick = function(){
 		callbackCancel();
